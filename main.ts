c90e87206dc4c8772ec10c77cd371d58a063be4f
Marcus Davage <spufidoo@users.noreply.github.com>
2023-01-19 22:36:47 +0000

    Update main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+})
 input.onPinPressed(TouchPin.P2, function () {
     for (let index = 0; index < 4; index++) {
         music.playTone(sound, music.beat(BeatFraction.Quarter))
         sound += -25
     }
     sound = 440
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
 })
 input.onPinPressed(TouchPin.P1, function () {
     for (let index = 0; index < 4; index++) {
